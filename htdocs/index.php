0a8ba6682780c0c7e08fa955794177f0e62bd126
Adrian Troconis <troconisbaltar@gmail.com>
2025-01-23 13:29:00 -0400

    Add debian archive repositories, changed some params for building

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,20 @@
 <?php
-echo '<h1>Hello World</h1>';
+    date_default_timezone_set('UTC'); // Set your desired timezone
+
+    $servername = "mysql";
+    $username = "root";
+    $password = "secret_password";
+    $dbname = "project1";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
+    } else {
+        die(json_encode(array('outcome' => true)));
+    }
+
+    $conn->close();
 ?>
